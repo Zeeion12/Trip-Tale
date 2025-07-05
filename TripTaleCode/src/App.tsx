1d53c06943f446262908610f6d0f8eccf7323fb3
Sulthan Syafiq Raihan <145461450+Sulthandfb@users.noreply.github.com>
2025-07-06 01:12:22 +0700

    Install Modul Sulthan

--- TripTaleCode/src/App.tsx
+++ TripTaleCode/src/App.tsx
@@ -5,7 +5,12 @@ function App() {
 
   return (
     <>
-
+      <div>
+        <h1 className='bg-amber-50'>TripTale</h1>
+        <p>Welcome to TripTale, your travel companion!</p>
+        <p>Explore the world with us.</p>
+        <p>Stay tuned for more features!</p>
+      </div>
     </>
   )
 }
